4bf39d6d6f46e3a1c9d0d3841698c4c0a6674adb
ranggo pato <patoranggo@gmail.com>
2024-10-07 16:27:57 +0700

    type inference and annotation

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,8 @@
 "use strict";
+// type inference
 let nama = "Ranggo";
+nama = "Pato";
+// type annotation
+let age = 26;
 console.log(nama);
+console.log(age);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+// type inference
 let nama = "Ranggo"
 
+nama = "Pato"
+
+// type annotation
+let age:number = 26
+
 console.log(nama)
+console.log(age)
