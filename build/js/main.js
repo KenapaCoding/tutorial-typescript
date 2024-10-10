e2318f209b580023746ee8a0dc301b7e965b0bf4
ranggo pato <patoranggo@gmail.com>
2024-10-10 10:28:48 +0700

    type-assertion

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,5 @@
 "use strict";
-let data;
-data = 24;
-if (typeof data === 'string') {
-    console.log(data.toUpperCase());
-}
-else {
-    console.log("data is not a string");
-}
+let someValue = "Hello TypeScript !";
+// console.log((someValue as string).length)
+let imgElement = document.getElementById("myImg");
+imgElement.src = "https://images.unsplash.com/photo-1721333088875-e9e3cc2c547a?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-let data : unknown
+let someValue:unknown = "Hello TypeScript !"
 
-data = 24
+// console.log((someValue as string).length)
 
-if(typeof data === 'string'){
-    console.log(data.toUpperCase())
-} else {
-    console.log("data is not a string")
-}
+let imgElement = document.getElementById("myImg") as HTMLImageElement
+
+imgElement.src = "https://images.unsplash.com/photo-1721333088875-e9e3cc2c547a?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
