3f89ae14a3e7c265c9ffd38aea96fbdeda9ca868
ranggo pato <patoranggo@gmail.com>
2024-10-12 05:53:51 +0700

    polymorphism

--- build/js/main.js
+++ build/js/main.js
@@ -1,36 +1,20 @@
 "use strict";
-class Product {
-    // public name:string; //data
-    // public price:number; //data
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-        this.name = name;
-        this.price = price;
-    }
-    displayProduct() {
-        console.log(`Product : ${this.name}, Price : $${this.price}`);
+class Animal {
+    makeSound() {
+        console.log("Some generic animal sound");
     }
 }
-class Electronic extends Product {
-    constructor(name, price, warranty) {
-        super(name, price);
-        this.warranty = warranty;
-    }
-    displayElectronic() {
-        console.log(`Warranty : ${this.warranty} years`);
-        super.displayProduct();
-        console.log(this.name);
+class Dog extends Animal {
+    makeSound() {
+        console.log('Woof woof !');
     }
 }
-const product1 = new Product("Laptop", 1500);
-const electronic1 = new Electronic("Smartphone", 1000, 2);
-console.log(electronic1);
-electronic1.displayElectronic();
-console.log(product1.name);
-product1.name = "Komputer";
-console.log(product1.name);
-// const product2 = new Product("Handphone", 1500)
-// const product3 = new Product("Mouse", 1500)
-// console.log(product1)
-// product1.displayProduct()
+class Cat extends Animal {
+    makeSound() {
+        console.log('Miaw miaw !');
+    }
+}
+const Pitbull = new Dog();
+const kucing = new Cat();
+Pitbull.makeSound();
+kucing.makeSound();
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,23 @@
-class Product{
-    // public name:string; //data
-    // public price:number; //data
-
-    constructor(public name:string, public price:number){
-        this.name = name
-        this.price = price
-    }
-
-    public displayProduct():void {
-        console.log(`Product : ${this.name}, Price : $${this.price}`)
+class Animal{
+    makeSound(){
+        console.log("Some generic animal sound")
     }
 }
 
-class Electronic extends Product {
-    warranty:number
-
-    constructor(name:string,price:number,warranty:number){
-        super(name,price)
-        this.warranty = warranty
-    }
-
-    displayElectronic():void {
-        console.log(`Warranty : ${this.warranty} years`)
-        super.displayProduct()
-        console.log(this.name)
+class Dog extends Animal{
+    makeSound(): void {
+        console.log('Woof woof !')
     }
 }
-const product1 = new Product("Laptop", 1500)
-const electronic1 = new Electronic("Smartphone", 1000, 2)
-console.log(electronic1)
 
-electronic1.displayElectronic()
-console.log(product1.name)
-product1.name = "Komputer"
-console.log(product1.name)
+class Cat extends Animal{
+    makeSound(): void {
+        console.log('Miaw miaw !')
+    }
+}
 
+const Pitbull = new Dog()
+const kucing = new Cat()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const product2 = new Product("Handphone", 1500)
-// const product3 = new Product("Mouse", 1500)
-
-// console.log(product1)
-// product1.displayProduct()
-
+Pitbull.makeSound()
+kucing.makeSound()
