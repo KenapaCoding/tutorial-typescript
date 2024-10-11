ee3ce5f688d7c27f47e1c77cb7d5f15a25925eb8
ranggo pato <patoranggo@gmail.com>
2024-10-11 14:21:22 +0700

    class, constructor, method, dan inheritance

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,28 @@
 "use strict";
-function processData(data, callback) {
-    data.forEach(callback);
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    displayProduct() {
+        console.log(`Product : ${this.name}, Price : $${this.price}`);
+    }
 }
-function logItem(item) {
-    console.log(item * 2);
+class Electronic extends Product {
+    constructor(name, price, warranty) {
+        super(name, price);
+        this.warranty = warranty;
+    }
+    displayElectronic() {
+        console.log(`Warranty : ${this.warranty} years`);
+        super.displayProduct();
+    }
 }
-processData([1, 2, 3, 4, 5], (item) => {
-    console.log(item * 2);
-});
+const electronic1 = new Electronic("Smartphone", 1000, 2);
+console.log(electronic1);
+electronic1.displayElectronic();
+// const product1 = new Product("Laptop", 1500)
+// const product2 = new Product("Handphone", 1500)
+// const product3 = new Product("Mouse", 1500)
+// console.log(product1)
+// product1.displayProduct()
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,54 @@
-function processData(data:number[], callback: (item:number) => void):void{
-    data.forEach(callback)
+class Product{
+    name:string; //data
+    price:number; //data
+
+    constructor(name:string, price:number){
+        this.name = name
+        this.price = price
+    }
+
+    displayProduct():void {
+        console.log(`Product : ${this.name}, Price : $${this.price}`)
+    }
 }
 
-function logItem(item:number):void{
-    console.log(item *2)
+class Electronic extends Product {
+    warranty:number
+
+    constructor(name:string,price:number,warranty:number){
+        super(name,price)
+        this.warranty = warranty
+    }
+
+    displayElectronic():void {
+        console.log(`Warranty : ${this.warranty} years`)
+        super.displayProduct()
+    }
 }
 
-processData([1,2,3,4,5],(item:number)=> {
-    console.log(item*2)
-})
+const electronic1 = new Electronic("Smartphone", 1000, 2)
+console.log(electronic1)
+
+electronic1.displayElectronic()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const product1 = new Product("Laptop", 1500)
+// const product2 = new Product("Handphone", 1500)
+// const product3 = new Product("Mouse", 1500)
+
+// console.log(product1)
+// product1.displayProduct()
+
